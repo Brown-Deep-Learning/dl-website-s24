cf77822efee984330f2a04aa306547b98c4b3b4f
Dylan Hu <git@dylan.hu>
2023-01-23 11:10:48 -0500

    dive in button scrolls to home

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,10 @@ const nav = document.getElementById("nav");
 menuButton?.addEventListener("click", () => {
   nav?.classList.toggle("hidden");
 });
+
+const navLinks = nav?.querySelectorAll("a");
+navLinks?.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav?.classList.toggle("hidden");
+  });
+});
