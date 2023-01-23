0f19b346edf00b8dda8a359ebda5cc66b234583e
Dylan Hu <git@dylan.hu>
2023-01-23 10:57:23 -0500

    add nav menu, improve mobile styling

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ import "./style.css";
 // import { initScene } from "./scene/scene";
 
 // initScene();
+
+const menuButton = document.getElementById("menu-button");
+const nav = document.getElementById("nav");
+menuButton?.addEventListener("click", () => {
+  nav?.classList.toggle("hidden");
+});
