3453fcf76592a34b7f8440be97efd30821fd63bf
Dylan Hu <git@dylan.hu>
2023-04-21 15:08:08 -0400

    progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [glsl()],
   base: "/dl-website-s23/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        dlday: resolve(__dirname, "pages/dlday/index.html"),
+      },
+    },
+  }
 });
